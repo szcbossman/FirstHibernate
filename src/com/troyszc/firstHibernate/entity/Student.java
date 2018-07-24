dd6c8705c8046b00afe6ee9042854e57e3d8c51c
szcbossman <songzicheng3@gmail.com>
2018-07-24 12:03:30 -0400

    PrimaryKeyDemo created to test the auto increment PK function

--- src/com/troyszc/firstHibernate/entity/Student.java
+++ src/com/troyszc/firstHibernate/entity/Student.java
@@ -1,15 +1,13 @@
 package com.troyszc.firstHibernate.entity;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Entity
 @Table(name = "student")
 public class Student {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) //handle auto-increment
     @Column(name = "id")
     private int id;
 
